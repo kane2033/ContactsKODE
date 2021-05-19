782c78c00eb49ea69ef73663e9329fcc9d37fe60
kane2033 <u-cent@yandex.ru>
2021-05-19 13:59:22 +0700

    Initial commit

---
+++ app/src/main/java/com/kode/contacts/AndroidApplication.kt
@@ -0,0 +1,21 @@
+package com.kode.contacts
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class AndroidApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@AndroidApplication)
+            modules(
+
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/kode/contacts/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kode.contacts.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kode.contacts.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
